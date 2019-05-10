509034970185f5d49084c2842c4c3d521088ca36
Cyro Guimaraes <cyro809@gmail.com>
2019-05-10 09:46:41 -0300

    Adds ondetemroda.com to ALLOWED_HOSTS.

--- ondetemroda/settings.py
+++ ondetemroda/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'w!kc5e%a)8+l8@)ky4#qkn+zp7i)hp0j7*@d9@_0!(u&=(d7bm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ondetemroda-env.6p3pcpjdgs.us-west-2.elasticbeanstalk.com', 'localhost']
+ALLOWED_HOSTS = [
+    'ondetemroda-env.6p3pcpjdgs.us-west-2.elasticbeanstalk.com',
+    'localhost',
+    'ondetemroda.com',
+    'www.ondetemroda.com'
+]
 
 
 # Application definition
