3ada9ec53b543c5fbb6fed4a9826829f9833bd51
Cyro Guimaraes <cyro809@gmail.com>
2019-06-12 15:19:02 -0300

    WIP: Google Maps prototype. Title for events list page.

--- events/views.py
+++ events/views.py
@@ -3,7 +3,7 @@ import requests
 
 
 def index(request):
-    r = requests.get('http://ec2-3-16-151-208.us-east-2.compute.amazonaws.com:3000/events')
+    r = requests.get('http://localhost:3000/events')
     events = r.json()
 
     return render(request, 'events/index.html', { 'events': events})
