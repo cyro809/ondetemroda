68e4963b4e72aa25b49079b0f5412f285e17431a
Cyro Guimaraes <cyro809@gmail.com>
2019-06-19 14:57:57 -0300

    WIP: Adding map on event list page.

--- events/static/js/map.js
+++ events/static/js/map.js
@@ -1,15 +1,8 @@
-function initMap() {
-  var locations = [
-    ['Bondi Beach', -33.890542, 151.274856, 4],
-    ['Coogee Beach', -33.923036, 151.259052, 5],
-    ['Cronulla Beach', -34.028249, 151.157507, 3],
-    ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
-    ['Maroubra Beach', -33.950198, 151.259302, 1]
-  ];
+function initMap(locations) {
 
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 10,
-    center: new google.maps.LatLng(-33.92, 151.25),
+    zoom: 18,
+    center: new google.maps.LatLng(locations[0][1], locations[0][2]),
     mapTypeId: google.maps.MapTypeId.ROADMAP
   });
 
--- events/views.py
+++ events/views.py
@@ -5,5 +5,14 @@ import requests
 def index(request):
     r = requests.get('http://localhost:3000/events')
     events = r.json()
+    locations = []
+    for event in events:
+        location = [
+            event.get('NomeEvento'),
+            event.get('Latitude'),
+            event.get('Longitude'),
+            1
+        ]
+        locations.append(location)
 
-    return render(request, 'events/index.html', { 'events': events})
+    return render(request, 'events/index.html', { 'events': events, 'locations': locations})
