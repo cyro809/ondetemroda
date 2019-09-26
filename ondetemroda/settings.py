d9f50446ab41ff3728f6ca576666753cd473988a
Cyro Guimaraes <cyro809@gmail.com>
2019-09-26 17:59:34 -0300

    Fix static_root.

--- ondetemroda/settings.py
+++ ondetemroda/settings.py
@@ -138,5 +138,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATIC_ROOT = '/opt/ondetemroda/static/'
+STATIC_ROOT = '/opt/ondetemroda/ondetemroda/static/'
 STATIC_URL = '/static/'
